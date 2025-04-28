00e0fe2da5a31cb1d58245a49e0e1fecb47b8436
Heymow <33814287+Heymow@users.noreply.github.com>
2025-04-28 22:26:59 +0200

    Écrasement de index.js avec du contenu vide

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Bienvenue de vos entrer SrV");
-});
-
-app.listen(3000, () => {
-  console.log("Le serveur journe sur port 3000.");
-});
+ 
